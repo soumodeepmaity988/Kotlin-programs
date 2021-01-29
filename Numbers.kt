be98e1ade93f119740f9dcb329e863d5dafc35ae
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-29 07:09:50 +0530

    How to represent numbers in Kotlin and what is the range and how to print

---
+++ Numbers.kt
@@ -0,0 +1,32 @@
+fun main() {
+    println("==============================================")
+    val a: Int=1000
+    val d:Double=100.00
+    val f:Float=100.00f
+    val l:Long=1000000004
+    val s:Short=10
+    val b:Byte=1
+    println("Your int value is $a")
+    println("Your int value is $d")
+    println("Your int value is $f")
+    println("Your int value is $l")
+    println("Your int value is $s")
+    println("Your int value is $b")
+    ///==========================
+    println("==============================================")
+    println("Maximum Integer value is ${Integer.MAX_VALUE}")
+    println("Minimum Integer value is ${Integer.MIN_VALUE}")
+    println("Maximum Double value is ${Double.MAX_VALUE}")
+    println("Minimum Double value is ${Double.MIN_VALUE}")
+    println("Maximum Float value is ${Float.MAX_VALUE}")
+    println("Minimum Float value is ${Float.MIN_VALUE}")
+    println("Maximum Long value is ${Long.MAX_VALUE}")
+    println("Minimum Long value is ${Long.MIN_VALUE}")
+    println("Maximum Short value is ${Short.MAX_VALUE}")
+    println("Minimum Short value is ${Short.MIN_VALUE}")
+    println("Maximum Byte value is ${Byte.MAX_VALUE}")
+    println("Minimum Byte value is ${Byte.MIN_VALUE}")
+    println("==============================================")
+
+
+}
