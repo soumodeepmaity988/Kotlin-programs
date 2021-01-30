9898500212fffa8030fda8fa0e87fd02d1c8698e
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-30 10:02:01 +0530

    another function added

--- Range_in_kotlin.kt
+++ Range_in_kotlin.kt
@@ -12,4 +12,12 @@ fun userInput() {
     val number =Integer.parseInt(readLine())
     val result=number in 1..5
     println(result)
+    userInput1()
+}
+fun userInput1(){
+    println("You are in userInput1 method")
+    println("Enter the value of number: ")
+    val number =Integer.parseInt(readLine())
+    val result=number in 1..5
+    println(result)
 }
